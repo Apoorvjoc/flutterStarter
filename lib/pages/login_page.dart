437ff03bbea17037bdc9f21919012d82c5883251
Apoorv <hlwapoorv@gmail.com>
2022-01-11 10:37:14 +0530

    validation _formKey done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,8 +9,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   String name = "";
   bool changeBtn = false;
+
+  goToHome(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    setState(() {
+      changeBtn = true;
+    });
+    await Future.delayed(Duration(milliseconds: 1010));
+    await Navigator.pushNamed(context, MyRoutes.homeRoute);
+    setState(() {
+      changeBtn = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,66 +47,67 @@ class _LoginPageState extends State<LoginPage> {
           Padding(
             padding:
                 const EdgeInsets.symmetric(vertical: 16.0, horizontal: 42.0),
-            child: Column(
-              children: [
-                TextFormField(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter Name", labelText: "Username"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username can't be empty";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      }),
+                  TextFormField(
+                    obscureText: true,
                     decoration: InputDecoration(
-                        hintText: "Enter Name", labelText: "Username"),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    }),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter Password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-
-                InkWell(
-                  onTap: () async {
-                    setState(() {
-                      changeBtn = true;
-                    });
-                    await Future.delayed(Duration(milliseconds: 1010));
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: changeBtn ? 50 : 150,
-                    height: 40,
-                    // color: Colors.deepPurple,
-                    alignment: Alignment.center,
-                    child: changeBtn
-                        ? Icon(
-                            Icons.done,
-                            color: Colors.white,
-                          )
-                        : Text(
-                            "Login",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 18),
-                          ),
-                    decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius:
-                            BorderRadius.circular(changeBtn ? 50 : 7)),
+                        hintText: "Enter Password", labelText: "Password"),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Password can't be empty :(";
+                      }
+                      if (value != null && value.length < 3) {
+                        return "Password must be greater than 3.";
+                      }
+                    },
                   ),
-                )
-
-                // ElevatedButton(
-                //   child: Text("Login"),
-                //   style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-                //   onPressed: () {
-                //     print("Btn Clicked");
-                //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                //   },
-                // )
-              ],
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  InkWell(
+                    onTap: () => goToHome(context),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeBtn ? 50 : 150,
+                      height: 40,
+                      // color: Colors.deepPurple,
+                      alignment: Alignment.center,
+                      child: changeBtn
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18),
+                            ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changeBtn ? 50 : 7)),
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
